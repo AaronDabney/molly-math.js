8d85020afbb59f5b47d47592715a04b8017efe8d
Aaron Dabney <aarongdabney@gmail.com>
2024-06-15 15:40:37 -0700

    comment removal

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { Quaternion } from "./quaternion";
 import { Vector2 } from "./vector2";
 import { Vector3 } from "./vector3";
 
-// let q = Quaternion.identity();
-
 window.Quaternion = Quaternion;
 window.Vector2 = Vector2;
 window.Vector3 = Vector3;
